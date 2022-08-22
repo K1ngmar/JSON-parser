03b2526248f0a223decee19a7d0deb69e5fcb944
Ingmar Kole <ikole@student.codam.nl>
2022-08-22 12:43:35 +0200

    feat: added makefile

---
+++ make_settings/settings.mk
@@ -0,0 +1,30 @@
+ifndef SETTINGS_MK
+SETTINGS_MK = 1
+
+NAME	= json_parser.out
+
+CFLAGS	= -Wall -Wextra -Werror -pedantic -Ofast -std=c++17
+IFLAGS	= -I$(IDIR) -I$(IDIR)/data
+LFLAGS	= 
+
+SDIR	= src
+ODIR	= obj
+IDIR	= includes
+
+ifdef DEBUG
+	CFLAGS += -g -D DEBUG
+endif
+
+ifdef LEAKS
+	CFLAGS += -D LEAKS
+endif
+
+ifdef FSANITIZE
+	CFLAGS += -D FSANITIZE
+	CFLAGS = -g -fsanitize=address
+endif
+
+include ./make_settings/src_files.mk
+OBJ := $(patsubst $(SDIR)/%.cpp,$(ODIR)/%.o,$(SRC))
+
+endif
